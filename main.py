829bcc34bce7645f67ae3e375219112aa189c5ea
donQuiote <92533181+donQuiote@users.noreply.github.com>
2025-01-24 19:05:10 +0100

    merging process

--- main.py
+++ main.py
@@ -33,63 +33,65 @@ load_data = False
 #################
 apply_strat = True
 #################
-strategize = False
+strategize = True
 #strategy parameters
-if mom:
-    strategy = momentum.momentum_price
-    parameters_mom = {
-        "short_window": 5,
-        "long_window": 200,
-        "plot": False
-    }
-    s = parameters_mom["short_window"]
-    l = parameters_mom["long_window"]
-    param_names = f"_s{s}_l{l}"
+STLT = [(50,2000),(200,4000)]
+for stlt in STLT:
+    if mom:
+        strategy = momentum.momentum_price
+        parameters_mom = {
+            "short_window": stlt[0],
+            "long_window": stlt[1],
+            "plot": False
+        }
+        s = parameters_mom["short_window"]
+        l = parameters_mom["long_window"]
+        param_names = f"_s{s}_l{l}"
 
 
-find_error = False
-excess_vol = False
-volatility = False
+    find_error = False
+    excess_vol = False
+    volatility = False
 
-if find_error:
-    ticker = 'LOW'
-    files_bbo, files_trade = utils.data_handler_polars.handle_files(ticker=ticker, year=[2007, 2005], month="*", force_return_list=True)
-    utils.data_handler_polars.read_data(files_bbo, files_trade, ticker=ticker)
+    if find_error:
+        ticker = 'LOW'
+        files_bbo, files_trade = utils.data_handler_polars.handle_files(ticker=ticker, year=[2007, 2005], month="*", force_return_list=True)
+        utils.data_handler_polars.read_data(files_bbo, files_trade, ticker=ticker)
 
-    # concatenated_df = utils.data_handler_polars.read_data(files_bbo=files_bbo, files_trade=files_trade, ticker=ticker)
+        # concatenated_df = utils.data_handler_polars.read_data(files_bbo=files_bbo, files_trade=files_trade, ticker=ticker)
 
-if plot_data:
-    # utils.easy_plotter.plot_tickers_dates(bbo=True)
-    ticker = 'RTN'
-    df_average = utils.easy_plotter.daily_average_volume(ticker)
-    utils.easy_plotter.plot_daily_average_volume_single_stock(df_average, ticker=ticker)
+    if plot_data:
+        # utils.easy_plotter.plot_tickers_dates(bbo=True)
+        ticker = 'RTN'
+        df_average = utils.easy_plotter.daily_average_volume(ticker)
+        utils.easy_plotter.plot_daily_average_volume_single_stock(df_average, ticker=ticker)
 
 
-#################
-# Loads the data and merges the bbo and trade files -> creation of cleaned data
-#################
-if load_data:
-    #print(f"Loading data for {", ".join(TICKERS)}")
+    #################
+    # Loads the data and merges the bbo and trade files -> creation of cleaned data
+    #################
+    if load_data:
+        #print(f"Loading data for {", ".join(TICKERS)}")
 
-    for idx, ticker in enumerate(TICKERS[::-1]):
-        print(ticker)
-        print("+"*214)
-        print(f"Handling file {ticker} ({idx+1}/{len(TICKERS)}).")
+        for idx, ticker in enumerate(TICKERS[::-1]):
+            print(ticker)
+            print("+"*214)
+            print(f"Handling file {ticker} ({idx+1}/{len(TICKERS)}).")
 
-        files_bbo, files_trade = utils.data_handler_polars.handle_files(ticker=ticker, year=YEARS, month=MONTHS, force_return_list=True)
-        concatenated_df = utils.data_handler_polars.read_data(files_bbo=files_bbo, files_trade=files_trade, ticker=ticker)
+            files_bbo, files_trade = utils.data_handler_polars.handle_files(ticker=ticker, year=YEARS, month=MONTHS, force_return_list=True)
+            concatenated_df = utils.data_handler_polars.read_data(files_bbo=files_bbo, files_trade=files_trade, ticker=ticker)
 
-#################
-# Apply a certain strategy on cleaned data -> creation of strategies daily returns
-#################
-if apply_strat:
-    apply_strategy(strategy=strategy, param_names=param_names)
+    #################
+    # Apply a certain strategy on cleaned data -> creation of strategies daily returns
+    #################
+    if apply_strat:
+        apply_strategy(strategy=strategy, param_names=param_names)
 
-#################
-# Creates a dataframe of daily returns for all tickers and dates available
-#################
-if strategize:
-    build_strat_df(strategy=strategy, param_names=param_names)
+    #################
+    # Creates a dataframe of daily returns for all tickers and dates available
+    #################
+    if strategize:
+        build_strat_df(strategy=strategy, param_names=param_names)
 
 #if mom:
     #parameters_mom = {"short_window": 100, "long_window": 1000, "plot": True}
