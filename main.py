38e6033a21f6da76191e977c24bdc93c08b628a7
Gustave Paul Besacier <124517291+gustavebesacier@users.noreply.github.com>
2025-01-24 15:22:13 +0100

    patch: data loader

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ MONTHS = "*"
 TICKERS = ['EXC', 'DVN', 'IBM', 'GD', 'DIS', 'MON', 'BAC', 'CVS', 'BMY', 'PEP', 'MCD', 'HNZ', 'GE', 'DOW', 'APA', 'AA', 'COP', 'WFC', 'WMT', 'UNP', 'FCX', 'TWX', 'GS', 'T', 'MDT', 'KFT', 'CL', 'ALL', 'DD', 'FDX', 'VZ', 'JNJ', 'NOV', 'HPQ', 'ORCL', 'WMB', 'V', 'AEP', 'XRX', 'EMC', 'HON', 'ABT', 'MMM', 'MSFT', 'HD', 'MO', 'COF', 'USB', 'PG', 'MA', 'UPS', 'MS', 'JPM', 'LOW', 'RTN', 'CVX', 'TXN', 'ETR', 'UTX', 'BA', 'LMT', 'WY', 'AVP', 'MRK', 'AXP', 'PM', 'SLB', 'PFE', 'WAG', 'SO', 'BK', 'F', 'UNH', 'EMR', 'XOM', 'BHI', 'OXY', 'TGT', 'NSC', 'KO', 'CAT', 'C', 'HAL', 'BAX', 'MET', 'NKE', 'S']
 data_root = "/Users/gustavebesacier/Library/Mobile Documents/com~apple~CloudDocs/Documents/HEC/EPFL MA III/Financial big data/project/data/clean/APA/2004/02_bbo_trade.csv"
 
-load_data = True
+load_data = False
 mom = False
 plot_data = False
+find_error = True
+
+if find_error:
+    ticker = 'LOW'
+    files_bbo, files_trade = utils.data_handler_polars.handle_files(ticker=ticker, year=[2007, 2005], month="*", force_return_list=True)
+    utils.data_handler_polars.read_data(files_bbo, files_trade, ticker=ticker)
+
+    # concatenated_df = utils.data_handler_polars.read_data(files_bbo=files_bbo, files_trade=files_trade, ticker=ticker)
 
 if plot_data:
     # utils.easy_plotter.plot_tickers_dates(bbo=True)
